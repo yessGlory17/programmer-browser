d68ec69a7ac1f8deb92c7fc9782dd702c0612387
ozgur <ozgurkurucan337@gmail.com>
2022-10-14 21:31:18 +0300

    action update

---
+++ public/scripts/install_wine.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo dpkg --add-architecture i386
+
+curl -fsSL  https://dl.winehq.org/wine-builds/Release.key | sudo apt-key add -
+
+sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
+
+sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
+sudo apt-get update
+
+sudo apt-get install -y --install-recommends winehq-stable
+sudo apt-get install -y winetricks
+sudo apt-get install -y winbind
